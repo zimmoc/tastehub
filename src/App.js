8ca0280e68d51069424db6d4ad4448e5f832035f
Hampus Hjortsberg <hampushjortsberg@gmail.com>
2024-06-15 12:34:23 +0000

    Add SignUpForm page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import css from './styles/App.module.css';
 import './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
     <div className={css.App}>
       <Switch>
         <Route exact path="/" render={() => <HomePage />} />
+        <Route exact path="/signup" render={() => <SignUpForm />} />
+        <Route render={() => <p>Page not found</p>} />
       </Switch>
     </div>
   );
