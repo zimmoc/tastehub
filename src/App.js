82f7c1207a0b5c8e8272267c79617b3118d2551c
Hampus Hjortsberg <hampushjortsberg@gmail.com>
2024-06-15 01:55:16 +0000

    Add react-bootstrap, react-router and initial file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import css from './styles/App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div className={css.App}>
+      <Switch>
+        <Route exact path="/" render={() => <HomePage />} />
+      </Switch>
     </div>
   );
 }
