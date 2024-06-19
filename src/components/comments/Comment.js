eb02dd9ec9fab82b30290ab8ea1b08ad759d0665
Hampus Hjortsberg <hampushjortsberg@gmail.com>
2024-06-19 06:40:42 +0000

    Add CommentEditForm component

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../../styles/Comment.module.css';
 import Avatar from '../Avatar';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
@@ -6,6 +6,7 @@ import { Media } from 'react-bootstrap';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { MoreDropdown } from '../MoreDropDown';
 import { axiosRes } from '../../api/axiosDefaults';
+import CommentEditForm from './CommentEditForm';
 
 const Comment = (props) => {
   const {
@@ -18,6 +19,7 @@ const Comment = (props) => {
     setRecipe,
     setComments,
   } = props;
+  const [showEditForm, setShowEditForm] = useState(false);
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
@@ -55,10 +57,24 @@ const Comment = (props) => {
             {owner.name ? owner.name : owner}
           </span>
           <span className={styles.Date}>{updated_at}</span>
-          <p>{content}</p>
+          {showEditForm ? (
+            <CommentEditForm
+              id={id}
+              profile_id={profile_id}
+              content={content}
+              profileImage={profile_image}
+              setComments={setComments}
+              setShowEditForm={setShowEditForm}
+            />
+          ) : (
+            <p>{content}</p>
+          )}
         </Media.Body>
-        {is_owner && (
-          <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete} />
+        {is_owner && !showEditForm && (
+          <MoreDropdown
+            handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete}
+          />
         )}
       </Media>
     </div>
--- src/components/comments/CommentEditForm.js
+++ src/components/comments/CommentEditForm.js
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const CommentEditForm = () => {
-  return <div>CommentEditForm</div>;
-};
+import Form from 'react-bootstrap/Form';
+import { axiosRes } from '../../api/axiosDefaults';
+
+import styles from '../../styles/CommentCreateEditForm.module.css';
+import btnStyles from '../../styles/Button.module.css';
+import appStyles from '../../styles/App.module.css';
+
+function CommentEditForm(props) {
+  const { id, content, setShowEditForm, setComments } = props;
+
+  const [formContent, setFormContent] = useState(content);
+
+  const handleChange = (event) => {
+    setFormContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.put(`/comments/${id}`, {
+        content: formContent.trim(),
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.map((comment) => {
+          return comment.id === id
+            ? {
+                ...comment,
+                content: formContent.trim(),
+                updated_at: 'now',
+              }
+            : comment;
+        }),
+      }));
+      setShowEditForm(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="pr-1">
+        <Form.Control
+          className={appStyles.CustomInput}
+          as="textarea"
+          value={formContent}
+          onChange={handleChange}
+          rows={2}
+        />
+      </Form.Group>
+      <div className="text-right">
+        <button
+          className={`${btnStyles.Button} ${btnStyles.Orange}`}
+          onClick={() => setShowEditForm(false)}
+          type="button">
+          cancel
+        </button>
+        <button
+          className={`${btnStyles.Button} ${btnStyles.Orange}`}
+          disabled={!content.trim()}
+          type="submit">
+          save
+        </button>
+      </div>
+    </Form>
+  );
+}
 
 export default CommentEditForm;
